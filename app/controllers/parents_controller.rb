813d4e542729b345d94aa422b353f369e84c94a1
Sandeep Tamhankar <sandman@electric-cloud.com>
2015-07-20 11:46:09 -0700

    Install devise, configure Parent to the the user account model.

--- app/controllers/parents_controller.rb
+++ app/controllers/parents_controller.rb
@@ -1,5 +1,6 @@
 class ParentsController < ApplicationController
   before_action :set_parent, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_parent!
 
   # GET /parents
   # GET /parents.json
