c436d4577e08a70053e84a1d2f537d15011c9cb4
Sandeep Tamhankar <sandman@electric-cloud.com>
2015-07-22 18:18:34 -0700

    Set up action-mailer with SendGrid. First attempt; unclear if it will work.

--- app/controllers/parents_controller.rb
+++ app/controllers/parents_controller.rb
@@ -27,6 +27,7 @@ class ParentsController < ApplicationController
   def create
     respond_to do |format|
       if @parent.save
+        UserMailer.welcome_email(@parent).deliver_later
         format.html { redirect_to @parent, notice: 'Parent was successfully created.' }
         format.json { render :show, status: :created, location: @parent }
       else
---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: "fairwoodit@gmail.com"
+  layout 'mailer'
+end
