de6e8b5015be22b893902877a80d77206a8fa92d
Sandeep Tamhankar <sandeep.tamhankar@datastax.com>
2016-08-14 11:41:00 -0700

    Add top-level redirects to /activities and /walkathon if a user's url is activities.fairwoodexplorer.org or walkathon.fairwoodexplorer.org

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,10 @@ class WelcomeController < ApplicationController
   layout 'home'
 
   def index
+    if request.host == 'activities.fairwoodexplorer.org'
+      redirect_to 'http://apps.fairwoodexplorer.org/activities'
+    elsif request.host == 'walkathon.fairwoodexplorer.org'
+      redirect_to 'http://apps.fairwoodexplorer.org/walkathon'
+    end
   end
 end
